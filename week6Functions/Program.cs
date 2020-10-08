ecfea4f34c824e6d8f548f45e85b428602fcb9c9
Opilane TTHK <opilane@tthk.ee>
2020-10-08 11:28:53 +0300

    first commit

---
+++ week6Functions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace week6Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParamSum(5, 5);
+            DisplayString("100");
+        }
+
+
+        public static void ParamSum(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+
+        public static void DisplayString(string someString)
+        {
+            Console.WriteLine(someString);
+        }
+
+    }
+}
